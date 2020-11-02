5f9fb3d29cfc4900cd88ba5c51dd61dd248c09b0
Aayush Dip Giri <invincibleaayu@.github.com>
2020-11-02 11:17:54 +0545

    rungee kutta updated ques no 9

--- rungeekutta2ndorder.py
+++ rungeekutta2ndorder.py
@@ -1,19 +1,27 @@
- 
-def dydx(x, y) : 
-  return (x**2+x)    
-def rungeKutta(x0, y0, x, h) :  
-      n = round((x - x0) / h)  
-      y = y0   
-    print('x0\ty0\tk1\tk2\tyn')
-    print('-----------------------------------')
-      for i in range(1, n + 1) : 
-        k1 = h * dydx(x0, y)   
-        k2 = h * dydx(x0 + 0.5 * h, y + 0.5 * k1)  
-        y = y + (1.0 / 6.0) * (k1 + 2 * k2)  
-        print('%.4f\t%.4f\t%0.4f\t%.4f\t%.4f'% (x0,y0,k1,k2,y) )
-        print('-----------------------------------')   
-        x0 = x0 + h   
-# x0 = 0 
-# y = 1   
-# x = 2  h = 0.2   
-rungeKutta(0, 1, 2, 0.2)  
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+def f(x, y):
+    return x**2+x
+x0 = 0
+y0 = 1
+intervals = np.linspace(0, 2, 11)
+h = intervals[1] - intervals[0]
+
+y = list()
+y.append(y0)
+for x in intervals[1:]:
+    x0 = x
+    k1 = h * f(x0, y[-1])
+    k2 = h * (f((x0+h), (y[-1]+k1)))
+    yi = y[-1] + 1/2*(k1+k2)
+    y.append(yi)
+
+df = pd.DataFrame()
+df["x"]= intervals
+df["y"] = y
+print(df)
+
+plt.plot(intervals, y)
+plt.title("Runge-Kutta 2nd order method ")
+plt.show()
